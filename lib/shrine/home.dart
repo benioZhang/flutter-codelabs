68636f1984733f410c88b62302c0d3756b2a2c59
zhangzhibin <314053215@qq.com>
2019-05-06 17:51:33 +0800

    add codelab : MDC 102 Flutter

--- lib/shrine/home.dart
+++ lib/shrine/home.dart
@@ -13,21 +13,107 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'model/product.dart';
+import 'model/products_repository.dart';
 
 class HomePage extends StatelessWidget {
-  // TODO: Make a collection of cards (102)
   // TODO: Add a variable for Category (104)
   @override
   Widget build(BuildContext context) {
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
     return Scaffold(
-      // TODO: Add app bar (102)
-      // TODO: Add a grid view (102)
-      body: Center(
-        child: Text('You did it!'),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('SHRINE'),
+        leading: IconButton(
+            tooltip: 'menu', // 长按提示
+            icon: Icon(
+              Icons.menu,
+              semanticLabel: 'menu', // 类似Android中的accessibilityLabel.
+            ),
+            onPressed: () {
+              print('menu button');
+            }),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              semanticLabel: 'search',
+            ),
+            onPressed: () {
+              print('Search button');
+            },
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.tune,
+              semanticLabel: 'filter',
+            ),
+            onPressed: () {
+              print('Filter button');
+            },
+          )
+        ],
+      ),
+      body: GridView.count(
+        crossAxisCount: 2, // 滚动方向称为主轴，非滚动轴称为交叉轴。指定2列
+        padding: EdgeInsets.all(16),
+        childAspectRatio: 8.0 / 9.0,
+        children: _buildGridCards(context),
       ),
       resizeToAvoidBottomInset: false, // 确保键盘不会改变主页或其小部件的大小
     );
+  }
+
+  List<Widget> _buildGridCards(BuildContext context) {
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
+    if (products == null || products.isEmpty) {
+      return const [];
+    }
+
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+        locale: Localizations.localeOf(context).toString());
+
+    return products
+        .map((product) => Card(
+              clipBehavior: Clip.antiAlias,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  AspectRatio(
+                    aspectRatio: 18 / 11,
+                    child: Image.asset(
+                      product.assetName,
+                      package: product.assetPackage,
+                      fit: BoxFit.fitWidth,
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(16, 12, 16, 8),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            product.name,
+                            maxLines: 1,
+                            style: theme.textTheme.title,
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            formatter.format(product.price),
+                            style: theme.textTheme.body2,
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ))
+        .toList();
   }
 }
